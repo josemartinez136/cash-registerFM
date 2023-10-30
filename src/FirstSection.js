9c8e107622644423669c135399f11c06da4cf936
Jose <Your Email>
2023-10-30 11:09:32 -0700

    first commit

---
+++ src/FirstSection.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FirstSection() {
+    return (
+        <div className="FirstSection">
+            <h3>Cash Register Dispenser</h3>
+        </div>
+    )
+
+}
